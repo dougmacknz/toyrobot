976ba85b1432c31405f003677a876a35665d2bbc
Doug MacKenzie <douglmackenzie@gmail.com>
2019-06-05 19:09:30 +1000

    Use pure functions as much as possible

--- toyrobot.js
+++ toyrobot.js
@@ -23,11 +23,11 @@ toyRobot.prototype.place = function(x, y, f) {
  */
 toyRobot.prototype.left = function() {
   // First check that robot has been placed
-  if (!toyRobot.hasBeenPlaced.call(this)) {
+  if (!toyRobot.hasBeenPlaced(this.position)) {
     return;
   }
 
-  this.position.f = toyRobot.changeDirection.call(this, "left");
+  this.position.f = toyRobot.changeDirection(this.position, "left");
 };
 
 /**
@@ -35,11 +35,11 @@ toyRobot.prototype.left = function() {
  */
 toyRobot.prototype.right = function() {
   // First check that robot has been placed
-  if (!toyRobot.hasBeenPlaced.call(this)) {
+  if (!toyRobot.hasBeenPlaced(this.position)) {
     return;
   }
 
-  this.position.f = toyRobot.changeDirection.call(this, "right");
+  this.position.f = toyRobot.changeDirection(this.position, "right");
 };
 
 /**
@@ -49,7 +49,7 @@ toyRobot.prototype.right = function() {
  */
 toyRobot.prototype.report = function() {
   // First check that robot has been placed
-  if (!toyRobot.hasBeenPlaced.call(this)) {
+  if (!toyRobot.hasBeenPlaced(this.position)) {
     return;
   }
 
@@ -61,24 +61,39 @@ toyRobot.prototype.report = function() {
  */
 toyRobot.prototype.move = function() {
   // First check that robot has been placed
-  if (!toyRobot.hasBeenPlaced.call(this)) {
+  if (!toyRobot.hasBeenPlaced(this.position)) {
     return;
   }
 
   // Check that the robot can move without falling off
-  if (!toyRobot.canMove.call(this)) {
+  if (!toyRobot.canMove(this.position)) {
     return;
   }
 
-  if (this.position.f === "W") {
-    this.position.x -= 1;
-  } else if (this.position.f === "N") {
-    this.position.y += 1;
-  } else if (this.position.f === "E") {
-    this.position.x += 1;
-  } else if (this.position.f === "S") {
-    this.position.y -= 1;
+  // And use the pure function to retrieve a new position for the robot based on the current position
+  this.position = toyRobot.moveOneSpot(this.position);
+};
+
+/**
+ * Pure function to move the robot's position
+ *
+ * @param {object} currentPosition
+ * @returns {object}
+ */
+toyRobot.moveOneSpot = function(currentPosition) {
+  let newPosition = Object.assign({}, currentPosition);
+
+  if (currentPosition.f === "W") {
+    newPosition.x -= 1;
+  } else if (currentPosition.f === "N") {
+    newPosition.y += 1;
+  } else if (currentPosition.f === "E") {
+    newPosition.x += 1;
+  } else if (currentPosition.f === "S") {
+    newPosition.y -= 1;
   }
+
+  return newPosition;
 };
 
 // Possible ways the robot can face
@@ -87,20 +102,24 @@ toyRobot.directions = ["N", "E", "S", "W"];
 /**
  * Checks if the robot has been placed on the table yet
  *
+ * @param {object} position
  * @return {boolean}
  */
-toyRobot.hasBeenPlaced = function() {
-  return Object.keys(this.position).length !== 0;
+toyRobot.hasBeenPlaced = function(position) {
+  return Object.keys(position).length !== 0;
 };
 
 /**
- * Changes the direction that the robot is facing, used for the 'left' and 'right' methods
+ * Pure function that returns a new position with a change of direction
+ * Used for the 'left' and 'right' methods
  *
+ * @param {object} currentPosition
  * @param {'left'|'right'} direction
+ * @returns {object}
  */
-toyRobot.changeDirection = function(direction) {
+toyRobot.changeDirection = function(currentPosition, direction) {
   // Get the index of the current direction in the directions array
-  const currentDirection = toyRobot.directions.indexOf(this.position.f);
+  const currentDirection = toyRobot.directions.indexOf(currentPosition.f);
 
   let finalValue;
   let otherValue;
@@ -125,15 +144,18 @@ toyRobot.changeDirection = function(direction) {
 
 /**
  * Checks that the robot can move without falling based on its current position and direction
+ *
+ * @param {object} position
+ * @returns {boolean}
  */
-toyRobot.canMove = function() {
-  if (this.position.f === "N" && this.position.y === 4) {
+toyRobot.canMove = function(position) {
+  if (position.f === "N" && position.y === 4) {
     return false;
-  } else if (this.position.f === "S" && this.position.y === 0) {
+  } else if (position.f === "S" && position.y === 0) {
     return false;
-  } else if (this.position.f === "W" && this.position.x === 0) {
+  } else if (position.f === "W" && position.x === 0) {
     return false;
-  } else if (this.position.f === "E" && this.position.x === 4) {
+  } else if (position.f === "E" && position.x === 4) {
     return false;
   }
 
